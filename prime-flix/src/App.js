a0d34f68401babc82a098e207f01cd07e751eb45
Julyanne Correia <julyanne.es@gmail.com>
2022-11-01 18:19:40 -0300

    FEAT: NOTIFICATION DEPENDECY

--- prime-flix/src/App.js
+++ prime-flix/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Movie from './pages/Movie';
 import Header from './components/Header';
@@ -8,6 +10,7 @@ import Favorites from './pages/Favorites';
 function App(){
   return(
     <BrowserRouter>
+      <ToastContainer autoClose={3000}/>
       <Header/>
       <Routes>
         <Route path='/' element={ <Home/> }/>
