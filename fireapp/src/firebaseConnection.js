77c125c539a0d53c0cbbed161d51716ce4747056
Julyanne Correia <julyanne.es@gmail.com>
2022-11-04 19:04:36 -0300

    firebase config

---
+++ fireapp/src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCGuUWOEUg_1ZE5uKwZnHOjN-Is1dnVeLQ",
+    authDomain: "cursoreact-239e1.firebaseapp.com",
+    projectId: "cursoreact-239e1",
+    storageBucket: "cursoreact-239e1.appspot.com",
+    messagingSenderId: "951802903752",
+    appId: "1:951802903752:web:7371a3f7082da01dc2b9fd",
+    measurementId: "G-4SX5BM3LCB"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
