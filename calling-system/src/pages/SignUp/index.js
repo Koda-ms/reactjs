e310cc8cd4fdea8f859cbc040945f9f87a0fbf55
Julyanne Correia <julyanne.es@gmail.com>
2022-11-14 18:26:32 -0300

    feat: singup page

---
+++ calling-system/src/pages/SignUp/index.js
@@ -0,0 +1,42 @@
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { AuthContext } from '../../contexts/userAuth';
+
+function SignUp(){
+    const[name, setName] = useState('');
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const { signUp } = useContext(AuthContext);
+
+    function handleSignUp(e){
+        e.preventDefault();
+        if(name !== '' && email !== '' && password !== ''){
+            signUp(name, email, password);
+        }
+        toast.success("Account created successfully!");
+        setName('');
+        setEmail('');
+        setPassword('');
+    }
+
+    return(
+        <div className='login-container'>
+            <div className='logo'>
+                <div className='logo-area'>
+                    <img src={require('../../assets/logo.png')} alt=''/>
+                </div>
+                <form onSubmit={handleSignUp} className='form'>
+                    <input type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input type='text' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button type='submit'>Sign Up</button>
+                </form>
+                <Link to='/'>Already have an account? Sign in here!</Link>
+            </div>
+        </div>
+    );
+
+}
+
+export default SignUp;
