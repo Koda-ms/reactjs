6d49a861d31fa679cb42c0aaca5cb31435364319
Julyanne Correia <julyanne.es@gmail.com>
2022-12-06 10:38:01 -0300

    feat: privateRoute and customers page

--- calling-system/src/App.js
+++ calling-system/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthProvider from './contexts/userAuth';
-import RouteWrapper from './routes/Route';
+//import RouteWrapper from './routes/Route';
+import PrivateRoutes from './routes/PrivateRoutes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Customers from './pages/Customers';
 
 function App(){
   return(
@@ -14,17 +16,22 @@ function App(){
       <BrowserRouter>
       <ToastContainer autoClose={3000}/>
         <Routes>
+          <Route element={ <PrivateRoutes/> }>
+            <Route element={ <Home/> } path='/home' exact/>
+            <Route element={ <Profile/> } path='/settings'/>
+            <Route element={ <Customers/> } path='/customers'/>
+          </Route>
           <Route path='/' element={ <SignIn/> } />
           <Route path='/signup' element={ <SignUp/> } />
           {/* REACT-ROUTER-DOM V6'S ROUTES DOESN'T WORK WITH COMPONENTS DIFFERENT FROM <Route> AFTER <Routes>
               SO THE PRIVATE PAGES GOTTA BE SETTED INSIDE THE element={} */}
-          <Route path='/home' element={ 
+          {/* <Route path='/home' element={ 
             //THE RouteWrapper IS NOT WORKING. GOTTA CHECK THAT LATER.
-            <RouteWrapper isPrivate>
+            <PrivateRoutes>
               <Home/>
-            </RouteWrapper>
-          }/>
-          <Route path='/settings' element={ <Profile/> }/>
+            </PrivateRoutes>
+          }/> */}
+          {/* <Route path='/settings' element={ <Profile/> }/> */}
         </Routes>
       </BrowserRouter>
     </AuthProvider>
