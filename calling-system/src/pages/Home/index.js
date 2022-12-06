0e8a01baf3b503559c65a564c6191fbc106e3224
Julyanne Correia <julyanne.es@gmail.com>
2022-12-06 12:04:55 -0300

    style: home page design

--- calling-system/src/pages/Home/index.js
+++ calling-system/src/pages/Home/index.js
@@ -1,18 +1,41 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/userAuth";
+
 import Header from '../../components/Header';
 import Title from "../../components/Title";
+import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import './home.css';
 
 function Home(){
-    const { signOut, loadingAuth } = useContext(AuthContext);
-   
-    function handleLogOut(){
-        signOut();
-    }
+    const[customers, setCustomers] = useState([]);
 
     return(
         <div>
             <Header/>
+
+            <div className="content">
+                <Title name='Callings'>
+                    <FiMessageSquare size={25}/>
+                </Title>
+                {/* IF THERE'S NO CALLINGS AVAILABLE IN THE SYSTEM */}
+                {customers.length === 0 ? (
+                   <div className='container dashboard'>
+                        <span>No callings found</span>
+                        <Link to='/newCall' className='new'>
+                            <FiPlus size={25} color='#FFF'/>
+                            New Call
+                        </Link>
+                    </div> 
+                ) : (
+                    <>
+                    <Link to='/newCall' className='new'>
+                        <FiPlus size={25} color='#FFF'/>
+                        New Call
+                    </Link>
+                    </>
+                )}
+                
+            </div>
         </div>
     );
 
