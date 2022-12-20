358fb04edd84c25090fc185eec84549e2bda38cf
Julyanne Correia <julyanne.es@gmail.com>
2022-12-20 20:05:19 -0300

    feat: table for the callings

--- calling-system/src/pages/Home/index.js
+++ calling-system/src/pages/Home/index.js
@@ -1,13 +1,13 @@
 
 import Header from '../../components/Header';
 import Title from "../../components/Title";
-import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import './home.css';
 
 function Home(){
-    const[customers, setCustomers] = useState([]);
+    const[customers, setCustomers] = useState([1]);
 
     return(
         <div>
@@ -32,6 +32,37 @@ function Home(){
                         <FiPlus size={25} color='#FFF'/>
                         New Call
                     </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope='col'>Client</th>
+                                <th scope='col'>Subject</th>
+                                <th scope='col'>Status</th>
+                                <th scope='col'>Signed in</th>
+                                <th scope='col'>#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label='Client'>Mário Nascimento</td>
+                                <td data-label='Subject'>Support</td>
+                                <td data-label='Status'>
+                                    <span className='badge' style={{backgroundColor: '#5cb85c'}}>Open</span>
+                                </td>
+                                <td data-label='Signed in'>12/20/2022</td>
+                                <td data-label='#'>
+                                    <button className='action' style={{backgroundColor: '#3583f6'}}>
+                                        <FiSearch size={17} color='#FFF'/>
+                                    </button>
+                                    <button className='action' style={{backgroundColor: '#F6a935'}}>
+                                        <FiEdit2 size={17} color='#FFF'/>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
                     </>
                 )}
                 
