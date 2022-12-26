835d6251dbe28ea56056e36085550b411f62e044
Julyanne Correia <julyanne.es@gmail.com>
2022-12-26 17:22:57 -0300

    feat: more info + modal

--- calling-system/src/pages/Home/index.js
+++ calling-system/src/pages/Home/index.js
@@ -1,13 +1,114 @@
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import firebase from '../../services/dbConnection';
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
+import { format } from 'date-fns';
 import Header from '../../components/Header';
 import Title from "../../components/Title";
-import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import Modal from '../../components/Modal';
 import './home.css';
 
+const listRef = firebase.firestore().collection('callings').orderBy('created', 'desc').limit(5);
+
 function Home(){
-    const[customers, setCustomers] = useState([1]);
+    const[customers, setCustomers] = useState([]);
+    const[loadCalls, setLoadCalls] = useState(true);
+    //THERE'S A LIMIT SET TO LOAD ONLY A INTERVAL OF CALLS, SO
+    //IF THERE'S MORE CALLS TO BE LOADED THIS IS SET TO true
+    const[loadMore, setLoadMore] = useState(false);
+    //THIS CONST DEFINES IF THE LIST IS EMPTY, BECAUSE IF IT IS
+    //THEN IT MEANS THERE'S NO CALL TO BE SEARCHED
+    const[isEmpty, setIsEmpty] = useState(false); 
+    const[lastDocs, setLastDocs] = useState();
+
+    const[postModal, setPostModal] = useState(false);
+    const[itemDetail, setItemDetail] = useState();
+
+    useEffect(() => {
+
+        async function loadCallings(){
+            await listRef.get()
+            .then((snapshot) => {
+                updateCalls(snapshot)
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoadCalls(false);
+            })
+        }
+        loadCallings();
+    }, []);
+
+    function updateCalls(snapshot){
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if(!isCollectionEmpty){
+            let list = [];
+
+            snapshot.forEach((doc) => {
+                list.push({
+                    id: doc.id,
+                    clientId: doc.data().clientId,
+                    client: doc.data().client,
+                    subject: doc.data().subject,
+                    status: doc.data().status,
+                    created: doc.data().created,
+                    formatedDate: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    complement: doc.data().complement
+                })
+            })
+            //THIS SERVES TO SET THE LAST DOC SHOWN ON THE LIST INSIDE THE LIMIT,
+            //THEN WHEN ANOTHER LIST GOES TO LOAD THE SERACH IN THE DB WILL 
+            //START RIGHT AFTER THIS DOC
+            const lastDoc = snapshot.docs[snapshot.docs.length - 1];
+            //THE SPREAD OPERATOR HELPS BECAUSE IF THE LIST HAS ANY OTHER CALL (...list) THEN,
+            //ON THE SEARCH THOSE NEW CALLS ARE SHOWN ALONG WITH THE "OLD" ONES (...customers)
+            setCustomers([...customers, ...list]);
+            setLastDocs(lastDoc);
+            setLoadCalls(false);
+
+        } else {
+            setIsEmpty(true);
+        }
+        setLoadMore(false);
+    }
+
+    //CALLED WHEN THERE'S MORE CALLINGS IN THE DB THAT CAN BE SHOWN
+    async function handleMore(){
+        setLoadMore(true);
+
+        await listRef.startAfter(lastDocs).limit(5).get()
+        .then((snapshot) => {
+            updateCalls(snapshot);
+            console.log(loadMore);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    function toggleModal(item){
+        setPostModal(!postModal);
+        setItemDetail(item);
+    }
+
+    if(loadCalls){
+        return(
+            <div>
+                <Header/>
+
+                <div className="content">
+                    <Title name='Callings'>
+                        <FiMessageSquare size={25}/>
+                    </Title>
+
+                    <div className='container dashboard'>
+                        <span>Searching calls...</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return(
         <div>
@@ -44,29 +145,40 @@ function Home(){
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td data-label='Client'>Mário Nascimento</td>
-                                <td data-label='Subject'>Support</td>
-                                <td data-label='Status'>
-                                    <span className='badge' style={{backgroundColor: '#5cb85c'}}>Open</span>
-                                </td>
-                                <td data-label='Signed in'>12/20/2022</td>
-                                <td data-label='#'>
-                                    <button className='action' style={{backgroundColor: '#3583f6'}}>
-                                        <FiSearch size={17} color='#FFF'/>
-                                    </button>
-                                    <button className='action' style={{backgroundColor: '#F6a935'}}>
-                                        <FiEdit2 size={17} color='#FFF'/>
-                                    </button>
-                                </td>
-                            </tr>
+                            {customers.map((item, index) => {
+                                return(
+                                    <tr key={index}>  
+                                        <td data-label='Client'>{item.client}</td>
+                                        <td data-label='Subject'>{item.subject}</td>
+                                        <td data-label='Status'>
+                                            <span className='badge' style={{backgroundColor: item.status === 'Open' ? '#5cb85c' : '#999'}}>{item.status}</span>
+                                        </td>
+                                        <td data-label='Signed in'>{item.formatedDate}</td>
+                                        <td data-label='#'>
+                                            <button className='action' style={{backgroundColor: '#3583f6'}} onClick={() => toggleModal(item)}>
+                                                <FiSearch size={17} color='#FFF'/>
+                                            </button>
+                                            <Link className='action' style={{backgroundColor: '#F6a935'}} to={`/newCall/${item.id}`}>
+                                                <FiEdit2 size={17} color='#FFF'/>
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
                         </tbody>
                     </table>
 
+                    { !loadMore && !isEmpty && <button className='btn-more' onClick={handleMore}>Search more</button> }
                     </>
                 )}
                 
             </div>
+
+            { postModal && (
+                <Modal content={itemDetail}
+                close={toggleModal}/>
+            )}
+
         </div>
     );
 
