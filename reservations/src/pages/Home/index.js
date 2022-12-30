e9483b231e088848c5073d9b3b5792f392962ed4
Julyanne Correia <julyanne.es@gmail.com>
2022-12-30 14:50:36 -0300

    feat: home page

---
+++ reservations/src/pages/Home/index.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import { MdFlightTakeoff } from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+import { addReserve } from '../../store/modules/reserve/actions';
+import api from '../../services/api';
+import './home.css'; 
+
+function Home() {
+    const[trips, setTrips] = useState([]);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        async function loadApi(){
+            const response = await api.get('trips');
+          
+            setTrips(response.data);
+        }
+
+        loadApi();
+    }, []);
+
+    //THIS FUNCTION SEND AN ACTION TO THE REDUCER.
+    //THE dispatch KEEPS THE ACTION CLASSIFIED BY
+    //ITS TYPE (STATE) AND THE CONTENT OF ITSELF.
+    function handleAdd(trip){
+        dispatch(addReserve(trip));
+    }
+
+    return(
+        <div className='box'>
+            {trips.map(trip => {
+                return(
+                    <ul key={trip.id}>
+                        <li>
+                            <img src={trip.image} alt=''/> 
+                            <strong>{trip.title}</strong>
+                            <span>Status: {trip.status ? 'Available' : 'Unavailable'}</span>
+
+                            <button type='button' onClick={()=> handleAdd(trip)}>
+                                <div>
+                                    <MdFlightTakeoff color= '#fff' size={14}/>
+                                </div>
+                                <span>MAKE RESERVATION</span>
+                            </button>
+                        
+                        </li>
+                    </ul>
+                );
+            })}
+        </div>
+
+    );
+}
+
+export default Home;
